5b656d81c68336f5853382e0029ffd60155611cf
Dhruv Chowdhary <dhruvchowdhary@berkeley.edu>
2023-05-05 03:47:29 -0700

    Initial Commit

---
+++ Check Split/Check_SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Check_SplitApp.swift
+//  Check Split
+//
+//  Created by Dhruv Chowdhary on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Check_SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
